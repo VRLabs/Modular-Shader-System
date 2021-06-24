14d2acdf07eed6b61892388476412632cfafe6d6
Cibbi <12004047+Cibbi@users.noreply.github.com>
2021-06-24 17:34:23 +0200

    Add shader data viewer window

--- Editor/ShaderGenerator.cs
+++ Editor/ShaderGenerator.cs
@@ -19,7 +19,7 @@ namespace VRLabs.ModularShaderSystem
         private List<string> _nonCgPropertyNames;
         private List<Property> _properties;
 
-        public void GenerateMainShader(string path, ModularShader shader)
+        public void GenerateMainShader(string path, ModularShader shader, bool hideVariants = false)
         {
             _shader = shader;
             var shaderFile = new StringBuilder();
@@ -36,11 +36,10 @@ namespace VRLabs.ModularShaderSystem
 
             EnablePropertyValue[] currentSettings = new EnablePropertyValue[_nonCgPropertyNames.Count];
 
-            var variants = GenerateVariantsRecursive(shaderFile, currentlyIteratedObject, currentSettings);
+            var variants = GenerateVariantsRecursive(shaderFile, currentlyIteratedObject, currentSettings, hideVariants);
 
             foreach ((string variantCode, StringBuilder shaderVariant) in variants)
             {
-                 
                 StringBuilder finalFile = CleanupShaderFile(shaderVariant);
                 File.WriteAllText($"{path}/{_shader.Name}{variantCode}.shader", finalFile.ToString());
             }
@@ -59,9 +58,6 @@ namespace VRLabs.ModularShaderSystem
             _shader = shader;
             var shaderFile = new StringBuilder();
             _modules = FindAllModules(_shader);
-            var PropertyEnablers = _modules
-                .Where(x => x.Enabled != null && !string.IsNullOrWhiteSpace(x.Enabled.Name))
-                .Select(x => x.Enabled).ToList();
 
             _properties = FindUsedProperties(_shader, enableProperties);
 
@@ -69,8 +65,8 @@ namespace VRLabs.ModularShaderSystem
 
             string suffix = string.Join("", enableProperties.Select(x => x.Value));
 
-            shaderFile.PrependLineTabbed(0, "{");
-            shaderFile.PrependLineTabbed(0, $"Shader \".hidden/opt/{_shader.ShaderPath}{suffix}\"");
+            shaderFile.PrependLine("{");
+            shaderFile.PrependLine($"Shader \"Hidden/opt/{_shader.ShaderPath}{suffix}\"");
 
             WriteProperties(shaderFile);
 
@@ -87,7 +83,7 @@ namespace VRLabs.ModularShaderSystem
             // Write Functions to shader
             WriteShaderFunctions(shaderFile, functions);
 
-            shaderFile.AppendLineTabbed(0, "}");
+            shaderFile.AppendLine("}");
 
             File.WriteAllText($"{_shader.Name}{suffix}.shader", shaderFile.ToString());
 
@@ -95,7 +91,7 @@ namespace VRLabs.ModularShaderSystem
             _properties = null;
         }
 
-        private List<(string, StringBuilder)> GenerateVariantsRecursive(StringBuilder shaderFile, int currentlyIteratedObject, EnablePropertyValue[] currentSettings)
+        private List<(string, StringBuilder)> GenerateVariantsRecursive(StringBuilder shaderFile, int currentlyIteratedObject, EnablePropertyValue[] currentSettings, bool isVariantHidden)
         {
             var files = new List<(string, StringBuilder)>();
 
@@ -103,11 +99,11 @@ namespace VRLabs.ModularShaderSystem
             if (currentlyIteratedObject >= _nonCgPropertyNames.Count)
             {
                 var variantShader = new StringBuilder(shaderFile.ToString());
-                files.Add(GenerateShaderVariant(variantShader, currentSettings));
+                files.Add(GenerateShaderVariant(variantShader, currentSettings, isVariantHidden));
                 return files;
             }
 
-            //Searches all possible values for the scurrent property enabler
+            // Searches all possible values for the current property enabler
             List<int> possibleValues = _nonCgPropertyEnablers.Where(x => x.Name.Equals(_nonCgPropertyNames[currentlyIteratedObject]))
                 .Select(x => x.EnableValue)
                 .Append(0)
@@ -115,14 +111,14 @@ namespace VRLabs.ModularShaderSystem
                 .OrderBy(x => x)
                 .ToList();
 
-            //Recursively calls this function once for every possible branch referencing the next property to branch into.
+            // Recursively calls this function once for every possible branch referencing the next property to branch into.
             // Then aggregates all the resulting variants from this branch of the tree call, to then return it to its root
             foreach (int value in possibleValues)
             {
                 var newSettings = new EnablePropertyValue[_nonCgPropertyNames.Count];
                 Array.Copy(currentSettings, newSettings, _nonCgPropertyNames.Count);
                 newSettings[currentlyIteratedObject] = new EnablePropertyValue { Name =_nonCgPropertyNames[currentlyIteratedObject], Value = value };
-                List<(string, StringBuilder)> returnFiles = GenerateVariantsRecursive(shaderFile, currentlyIteratedObject + 1, newSettings);
+                List<(string, StringBuilder)> returnFiles = GenerateVariantsRecursive(shaderFile, currentlyIteratedObject + 1, newSettings, isVariantHidden);
 
                 files.AddRange(returnFiles);
             }
@@ -130,17 +126,23 @@ namespace VRLabs.ModularShaderSystem
             return files;
         }
 
-        private (string, StringBuilder) GenerateShaderVariant(StringBuilder shaderFile, EnablePropertyValue[] currentSettings)
+        private (string, StringBuilder) GenerateShaderVariant(StringBuilder shaderFile, EnablePropertyValue[] currentSettings, bool isVariantHidden)
         {
-            string suffix = string.Join("", currentSettings.Select(x => x.Value));
+            string suffix = "";
+            if (currentSettings.Any(x => x.Value != 0))
+                suffix = string.Join("", currentSettings.Select(x => x.Value));
 
             var variantEnabledModules = _modules
                 .Where(x => x != null)
                 .Where(x => x.Enabled == null || string.IsNullOrWhiteSpace(x.Enabled.Name) || currentSettings.Select(y => y.Name).Contains(x.Enabled.Name));
 
             // Add Shader Location
-            shaderFile.PrependLineTabbed(0, "{");
-            shaderFile.PrependLineTabbed(0, $"Shader \"Hidden/{_shader.ShaderPath}{suffix}\"");
+            shaderFile.PrependLine("{");
+            
+            if(isVariantHidden)
+                shaderFile.PrependLine($"Shader \"Hidden/{_shader.ShaderPath}{suffix}\"");
+            else
+                shaderFile.PrependLine($"Shader \"{_shader.ShaderPath}{suffix}\"");
 
             // Write shader skeleton from templates 
             WriteShaderSkeleton(shaderFile, currentSettings);
@@ -157,8 +159,8 @@ namespace VRLabs.ModularShaderSystem
             WriteShaderFunctions(shaderFile, functions);
 
             if(!string.IsNullOrWhiteSpace(_shader.CustomEditor))
-                shaderFile.AppendLineTabbed(1, $"CustomEditor \"{_shader.CustomEditor}\"");
-            shaderFile.AppendLineTabbed(0, "}");
+                shaderFile.AppendLine($"CustomEditor \"{_shader.CustomEditor}\"");
+            shaderFile.AppendLine("}");
 
             
             MatchCollection m = Regex.Matches(shaderFile.ToString(), @"#K#.*$", RegexOptions.Multiline);
@@ -186,12 +188,12 @@ namespace VRLabs.ModularShaderSystem
                 .OrderBy(x => x.Type))
             {
                 if (variable.Type.Equals("sampler2D") || variable.Type.Equals("Texture2D"))
-                    variablesDeclaration.AppendLineTabbed(2, $"{variable.Type} {variable.Name}; float4 {variable.Name}_ST;");
+                    variablesDeclaration.AppendLine($"{variable.Type} {variable.Name}; float4 {variable.Name}_ST;");
                 else
-                    variablesDeclaration.AppendLineTabbed(2, $"{variable.Type} {variable.Name};");
+                    variablesDeclaration.AppendLine($"{variable.Type} {variable.Name};");
             }
 
-            variablesDeclaration.AppendLineTabbed(2, "#K#" + sink);
+            variablesDeclaration.AppendLine("#K#" + sink);
             shaderFile.Replace("#K#" + sink, variablesDeclaration.ToString());
         }
 
@@ -208,10 +210,10 @@ namespace VRLabs.ModularShaderSystem
                 tabs = WriteFunctionCallSequence(functions, function, module, functionCode, functionCallSequence, tabs);
                 string codeSink = string.IsNullOrWhiteSpace(function.CodeSinkKeyword) ? DEFAULT_CODE_SINK : function.CodeSinkKeyword;
 
-                functionCode.AppendLineTabbed(tabs, "#K#" + codeSink);
+                functionCode.AppendLine("#K#" + codeSink);
                 shaderFile.Replace("#K#" + codeSink, functionCode.ToString());
 
-                functionCallSequence.AppendLineTabbed(tabs, function.AppendAfter);
+                functionCallSequence.AppendLine(function.AppendAfter);
                 shaderFile.Replace(function.AppendAfter, functionCallSequence.ToString());
             }
         }
@@ -219,22 +221,22 @@ namespace VRLabs.ModularShaderSystem
         private static int WriteFunctionCallSequence(List<ShaderFunction> functions, ShaderFunction function, ShaderModule module, 
             StringBuilder functionCode, StringBuilder functionCallSequence, int tabs)
         {
-            functionCode.AppendMultilineTabbed(tabs, function.ShaderFunctionCode.Template);
+            functionCode.AppendLine(function.ShaderFunctionCode.Template);
 
             if (module.Enabled != null && !string.IsNullOrWhiteSpace(module.Enabled.Name))
             {
-                functionCallSequence.AppendLineTabbed(tabs, $"if({module.Enabled.Name} == {module.Enabled.EnableValue})");
-                functionCallSequence.AppendLineTabbed(tabs, "{");
+                functionCallSequence.AppendLine($"if({module.Enabled.Name} == {module.Enabled.EnableValue})");
+                functionCallSequence.AppendLine("{");
                 tabs++;
-                functionCallSequence.AppendLineTabbed(tabs, $"{function.Name}();");
+                functionCallSequence.AppendLine($"{function.Name}();");
                 foreach (var fn in functions.Where(x => x.AppendAfter.Equals(function.Name)).OrderBy(x => x.Priority))
                     WriteFunctionCallSequence(functions, fn, module, functionCode, functionCallSequence, tabs);
 
-                functionCallSequence.AppendLineTabbed(tabs, "}");
+                functionCallSequence.AppendLine("}");
             }
             else
             {
-                functionCallSequence.AppendLineTabbed(tabs, $"{function.Name}();");
+                functionCallSequence.AppendLine($"{function.Name}();");
                 foreach (var fn in functions.Where(x => x.AppendAfter.Equals(function.Name)).OrderBy(x => x.Priority))
                     WriteFunctionCallSequence(functions, fn, module, functionCode, functionCallSequence, tabs);
             }
@@ -244,30 +246,32 @@ namespace VRLabs.ModularShaderSystem
 
         private void WriteShaderSkeleton(StringBuilder shaderFile, IEnumerable<EnablePropertyValue> currentSettings)
         {
-            shaderFile.AppendLineTabbed(1, "SubShader");
-            shaderFile.AppendLineTabbed(1, "{");
+            shaderFile.AppendLine("SubShader");
+            shaderFile.AppendLine("{");
 
             using (var sr = new StringReader(_shader.ShaderTemplate.Template))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
-                    shaderFile.AppendLineTabbed(2, line);
+                    shaderFile.AppendLine(line);
             }
 
             WriteModuleTemplates(shaderFile, currentSettings);
         }
 
-        private static List<ShaderModule> FindAllModules(ModularShader shader)
+        public static List<ShaderModule> FindAllModules(ModularShader shader)
         {
             List<ShaderModule> modules = new List<ShaderModule>();
+            if (shader == null) return modules;
             modules.AddRange(shader.BaseModules);
             modules.AddRange(shader.AdditionalModules);
             return modules;
         }
 
-        private static List<ShaderFunction> FindAllFunctions(ModularShader shader)
+        public static List<ShaderFunction> FindAllFunctions(ModularShader shader)
         {
             var functions = new List<ShaderFunction>();
+            if (shader == null) return functions;
             foreach (var module in shader.BaseModules)
                 functions.AddRange(module.Functions);
 
@@ -276,9 +280,10 @@ namespace VRLabs.ModularShaderSystem
             return functions;
         }
 
-        private static List<ModuleTemplate> FindAllTemplates(ModularShader shader)
+        public static List<ModuleTemplate> FindAllTemplates(ModularShader shader)
         {
             var templates = new List<ModuleTemplate>();
+            if (shader == null) return templates;
             foreach (var module in shader.BaseModules)
                 templates.AddRange(module.Templates);
 
@@ -287,9 +292,11 @@ namespace VRLabs.ModularShaderSystem
             return templates;
         }
 
-        private static List<Property> FindAllProperties(ModularShader shader)
+        public static List<Property> FindAllProperties(ModularShader shader)
         {
+            
             List<Property> properties = new List<Property>();
+            if (shader == null) return properties;
 
             properties.AddRange(shader.Properties);
 
@@ -310,7 +317,7 @@ namespace VRLabs.ModularShaderSystem
             return properties.Distinct().ToList();
         }
 
-        private static List<Property> FindUsedProperties(ModularShader shader, IEnumerable<EnablePropertyValue> values)
+        public static List<Property> FindUsedProperties(ModularShader shader, IEnumerable<EnablePropertyValue> values)
         {
             List<Property> properties = new List<Property>();
 
@@ -335,24 +342,24 @@ namespace VRLabs.ModularShaderSystem
                 {
                     var tmp = new StringBuilder();
 
-                    tmp.AppendLineTabbed(2, "#K#" + template.Keyword);
+                    tmp.AppendLine("#K#" + template.Keyword);
                     shaderFile.Replace("#K#" + template.Keyword, tmp.ToString());
                 }
             }
 
-            shaderFile.AppendLineTabbed(1, "}");
+            shaderFile.AppendLine("}");
         }
 
         private void WriteProperties(StringBuilder shaderFile)
         {
-            shaderFile.AppendLineTabbed(1, "Properties");
-            shaderFile.AppendLineTabbed(1, "{");
+            shaderFile.AppendLine("Properties");
+            shaderFile.AppendLine("{");
 
             foreach (var prop in _properties)
             {
-                shaderFile.AppendLineTabbed(2, $"{prop.Attributes} {prop.Name}(\"{prop.DisplayName}\", {prop.Type}) = {prop.DefaultValue}");
+                shaderFile.AppendLine($"{prop.Attributes} {prop.Name}(\"{prop.DisplayName}\", {prop.Type}) = {prop.DefaultValue}");
             }
-            shaderFile.AppendLineTabbed(1, "}");
+            shaderFile.AppendLine("}");
         }
         
         private static StringBuilder CleanupShaderFile(StringBuilder shaderVariant)
---
+++ Editor/Windows/ModularShaderDataViewer.cs
@@ -0,0 +1,179 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace VRLabs.ModularShaderSystem
+{
+    public class ModularShaderDataViewer : EditorWindow
+    {
+        [MenuItem("VRLabs/Modular Shader/Modular shader data view")]
+        public static void ShowExample()
+        {
+            ModularShaderDataViewer wnd = GetWindow<ModularShaderDataViewer>();
+            wnd.titleContent = new GUIContent("ModularShaderDataViewer");
+        }
+        
+        private ObjectField _modularShaderField;
+        private Foldout _propertiesFoldout;
+        private Foldout _variablesFoldout;
+        private Foldout _functionsFoldout;
+        private ModularShader _modularShader;
+
+        private VisualTreeAsset _propertyViewUxml;
+        private VisualTreeAsset _functionViewUxml;
+
+        public void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // Import UXML
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/VRLabs/ModularShaderSystem/Editor/Windows/ModularShaderDataViewer.uxml");
+            _propertyViewUxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/VRLabs/ModularShaderSystem/Editor/Windows/PropertyView.uxml");
+            _functionViewUxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/VRLabs/ModularShaderSystem/Editor/Windows/FunctionView.uxml");
+            VisualElement UxmlBase = visualTree.CloneTree();
+            root.Add(UxmlBase);
+            
+            // Query all needed fields
+            _modularShaderField = root.Q<ObjectField>("ModularShaderField");
+            _propertiesFoldout = root.Q<Foldout>("PropertiesFoldout");
+            _variablesFoldout = root.Q<Foldout>("VariablesFoldout");
+            _functionsFoldout = root.Q<Foldout>("FunctionsFoldout");
+            
+            // Setup object picker
+            _modularShaderField.objectType = typeof(ModularShader);
+            _modularShaderField.RegisterCallback<ChangeEvent<UnityEngine.Object>>(e =>
+            {
+                if (_modularShaderField.value != null)
+                {
+                    // 4
+                    _modularShader = (ModularShader)_modularShaderField.value;
+                    //_modularShaderSerialized = new SerializedObject(_modularShader);
+                }
+                else
+                {
+                    _modularShader = null;
+                }
+
+                PopulateLists();
+            
+            });
+            
+            // Add stylesheet
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/VRLabs/ModularShaderSystem/Editor/Windows/ModularShaderDataViewer.uss");
+            root.styleSheets.Add(styleSheet);
+        }
+
+        private void PopulateLists()
+        {
+            // Update Properties
+            _propertiesFoldout.Clear();
+            var properties = ShaderGenerator.FindAllProperties(_modularShader);
+            foreach (Property property in properties)
+            {
+                VisualElement p = _propertyViewUxml.CloneTree();
+                var name = p.Q<Label>("PropertyLabel");
+                var type = p.Q<Label>("TypeLabel");
+                name.text = property.Name;
+                type.text = property.Type;
+                
+                _propertiesFoldout.Add(p);
+                
+            }
+            
+            // Fetch all functions
+            var functions = ShaderGenerator.FindAllFunctions(_modularShader);
+            
+            // Update variables
+            _variablesFoldout.Clear();
+            GetVariables(functions, ShaderGenerator.DEFAULT_VARIABLES_SINK, true);
+            foreach (var sink in functions.Select(x => x.VariableSinkKeyword).Distinct().Where(x => !string.IsNullOrEmpty(x) && !x.Equals(ShaderGenerator.DEFAULT_VARIABLES_SINK)))
+                GetVariables(functions, sink);
+            
+            // Update Functions
+            _functionsFoldout.Clear();
+            foreach (var functionsGroup in functions.Where(x => x.AppendAfter.StartsWith("#K#")).GroupBy(x => x.AppendAfter))
+            {
+                VisualElement p = _functionViewUxml.CloneTree();
+                var name = p.Q<Label>("FunctionLabel");
+                var foldout = p.Q<Foldout>("FoldoutArea");
+                foldout.text = functionsGroup.Key;
+                name.RemoveFromHierarchy();
+                _functionsFoldout.Add(p);
+
+                foreach (ShaderFunction function in functionsGroup.OrderBy(x => x.Priority))
+                    CreateFunctionsHierarchy(functions, function, foldout);   
+                
+            }
+
+        }
+        
+        private void GetVariables(List<ShaderFunction> functions, string sink, bool isDefaultSink = false)
+        {
+            var variables = functions
+                .Where(x => x.VariableSinkKeyword.Equals(sink) || (isDefaultSink && string.IsNullOrWhiteSpace(x.VariableSinkKeyword)))
+                .SelectMany(x => x.UsedVariables)
+                .Distinct()
+                .OrderBy(x => x.Type).ToList();
+            if (variables.Count == 0) return;
+            var foldout = new Foldout{  text = $"#K#{sink}" };
+            
+            foreach (var variable in variables)
+            {
+                VisualElement p = _propertyViewUxml.CloneTree();
+                var name = p.Q<Label>("PropertyLabel");
+                var type = p.Q<Label>("TypeLabel");
+                name.text = variable.Name;
+                type.text = variable.Type;
+                foldout.Add(p);
+                
+                if (variable.Type.Equals("sampler2D") || variable.Type.Equals("Texture2D"))
+                {
+                    VisualElement p2 = _propertyViewUxml.CloneTree();
+                    var name2 = p.Q<Label>("PropertyLabel");
+                    var type2 = p.Q<Label>("TypeLabel");
+                    name2.text = $"{variable.Name}_ST";
+                    type2.text = "float4";
+                    foldout.Add(p2);
+                }
+            }
+
+            _variablesFoldout.Add(foldout);
+        }
+        
+        private void CreateFunctionsHierarchy(List<ShaderFunction> functions, ShaderFunction function, VisualElement parent)
+        {
+            var subFunctions = functions.Where(x => x.AppendAfter.Equals(function.Name)).OrderBy(x => x.Priority).ToList();
+            if (subFunctions.Count > 0)
+            {
+                VisualElement p = _functionViewUxml.CloneTree();
+                var name = p.Q<Label>("FunctionLabel");
+                var priority = p.Q<Label>("PriorityLabel");
+                var foldout = p.Q<Foldout>("FoldoutArea");
+                foldout.text = function.Name;
+                name.RemoveFromHierarchy();
+                priority.text = function.Priority.ToString();
+                parent.Add(p);
+                
+                foreach (var fn in functions.Where(x => x.AppendAfter.Equals(function.Name)).OrderBy(x => x.Priority))
+                    CreateFunctionsHierarchy(functions, fn, foldout);
+            }
+            else
+            {
+                VisualElement p = _functionViewUxml.CloneTree();
+                var name = p.Q<Label>("FunctionLabel");
+                var priority = p.Q<Label>("PriorityLabel");
+                var foldout = p.Q<Foldout>("FoldoutArea");
+                foldout.RemoveFromHierarchy();
+                name.text = function.Name;
+                priority.text = function.Priority.ToString();
+                
+                parent.Add(p);
+            }
+        }
+    }
+}
