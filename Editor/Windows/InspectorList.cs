3e76bcd3541aa4eb1585b791c125f1bbc41b3347
Cibbi <12004047+Cibbi@users.noreply.github.com>
2021-09-28 14:32:57 +0200

    Added drag and drop to inspectorList

--- Editor/Windows/InspectorList.cs
+++ Editor/Windows/InspectorList.cs
@@ -1,13 +1,20 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace VRLabs.ModularShaderSystem
 {
+    public interface IInspectorList
+    {
+        InspectorListItem draggedElement { get; set; }
+        void HighlightDrops();
+        void DeHighlightDrops();
+    }
 
     // Shamelessly taken from here: https://forum.unity.com/threads/custom-bindableelement.989693/    
-    public class InspectorList : BindableElement
+    public class InspectorList : BindableElement, IInspectorList
     {
         Foldout _listContainer;
         Button _addButton;
@@ -16,23 +23,77 @@ namespace VRLabs.ModularShaderSystem
 
         private bool _hasFoldingBeenForced;
 
+        public InspectorListItem draggedElement { get; set; }
+        public bool _highlightDrops;
+
+        private List<VisualElement> _drops;
+
+        private VisualElement _currentDrop;
+
         public InspectorList()
         {
+            _drops = new List<VisualElement>();
             _listContainer = new Foldout();
             _listContainer.text = "Unbound List";
             _listContainer.contentContainer.AddToClassList("inspector-list-container");
             _listContainer.value = false;
+            _listContainer.RegisterCallback<MouseUpEvent>(e => Drop());
+            _listContainer.RegisterCallback<MouseLeaveEvent>(e => Drop());
+
             _addButton = new Button(AddItem);
             _addButton.text = "Add";
             _addButton.AddToClassList("inspector-list-add-button");
             Add(_listContainer);
-            if(enabledSelf)
+            if (enabledSelf)
                 _listContainer.Add(_addButton);
             _listContainer.RegisterValueChangedCallback((e) => _array.isExpanded = e.newValue);
             var styleSheet = Resources.Load<StyleSheet>(MSSConstants.RESOURCES_FOLDER + "/MSSUIElements/InspectorList");
             styleSheets.Add(styleSheet);
         }
-     
+
+        private void Drop()
+        {
+            if (draggedElement == null) return;
+            draggedElement.RemoveFromClassList("inspector-list-drag-enabled");
+
+            if (_highlightDrops)
+            {
+                DeHighlightDrops();
+                int dropIndex = _drops.IndexOf(_currentDrop);
+
+                if (dropIndex == -1)
+                {
+                    draggedElement = null;
+                    return;
+                }
+
+                if (dropIndex > draggedElement.index) dropIndex--;
+                _array.MoveArrayElement(draggedElement.index, dropIndex);
+                bool expanded = _array.GetArrayElementAtIndex(dropIndex).isExpanded;
+                _array.GetArrayElementAtIndex(dropIndex).isExpanded = _array.GetArrayElementAtIndex(draggedElement.index).isExpanded;
+                _array.GetArrayElementAtIndex(draggedElement.index).isExpanded = expanded;
+                _array.serializedObject.ApplyModifiedProperties();
+                UpdateList();
+            }
+            draggedElement = null;
+        }
+
+        public void HighlightDrops()
+        {
+            foreach (var item in _drops)
+                item.AddToClassList("inspector-list-drop-area-highlight");
+
+            _highlightDrops = true;
+        }
+
+        public void DeHighlightDrops()
+        {
+            foreach (var item in _drops)
+                item.RemoveFromClassList("inspector-list-drop-area-highlight");
+
+            _highlightDrops = false;
+        }
+
         // Get the reference to the bound serialized object.
         public override void HandleEvent(EventBase evt)
         {
@@ -51,54 +112,75 @@ namespace VRLabs.ModularShaderSystem
             }
             base.HandleEvent(evt);
         }
-     
+
         // Refresh/recreate the list.
         public void UpdateList()
         {
             _listContainer.Clear();
-           
+            _drops.Clear();
+
             if (_array == null)
                 return;
             _listContainer.text = _array.displayName;
+            CreateDrop();
             for (int i = 0; i < _array.arraySize; i++)
             {
                 int index = i;
                 var element = new PropertyField(_array.GetArrayElementAtIndex(index));
                 element.Bind(_array.GetArrayElementAtIndex(index).serializedObject);
-                var item = new InspectorListItem(element, _array, index, _showElementsButtons);
-                item.removeButton.RegisterCallback<PointerUpEvent>((evt) => {
-                    RemoveItem(index);
-                });
-                item.upButton.RegisterCallback<PointerUpEvent>((evt) =>
-                {
-                    MoveUpItem(index);
-                });
-                item.downButton.RegisterCallback<PointerUpEvent>((evt) => {
-                    MoveDownItem(index);
-                });
+                var item = new InspectorListItem(this, element, _array, index, _showElementsButtons);
+                item.removeButton.RegisterCallback<PointerUpEvent>((evt) => RemoveItem(index));
+                item.upButton.RegisterCallback<PointerUpEvent>((evt) => MoveUpItem(index));
+                item.downButton.RegisterCallback<PointerUpEvent>((evt) => MoveDownItem(index));
                 _listContainer.Add(item);
+                CreateDrop();
             }
-            if(enabledSelf)
+            if (enabledSelf)
                 _listContainer.Add(_addButton);
         }
-     
+
+        private void CreateDrop()
+        {
+            VisualElement dropArea = new VisualElement();
+            dropArea.AddToClassList("inspector-list-drop-area");
+            dropArea.RegisterCallback<MouseEnterEvent>(e =>
+            {
+                if (_highlightDrops)
+                {
+                    dropArea.AddToClassList("inspector-list-drop-area-selected");
+                    _currentDrop = dropArea;
+                }
+            });
+            dropArea.RegisterCallback<MouseLeaveEvent>(e =>
+            {
+                if (_highlightDrops)
+                {
+                    dropArea.RemoveFromClassList("inspector-list-drop-area-selected");
+                    if (_currentDrop == dropArea) _currentDrop = null;
+                }
+            });
+
+            _listContainer.Add(dropArea);
+            _drops.Add(dropArea);
+        }
+
         // Remove an item and refresh the list
         public void RemoveItem(int index)
         {
-            if(_array != null)
+            if (_array != null)
             {
-                if(index < _array.arraySize - 1)
+                if (index < _array.arraySize - 1)
                     _array.GetArrayElementAtIndex(index).isExpanded = _array.GetArrayElementAtIndex(index + 1).isExpanded;
                 _array.DeleteArrayElementAtIndex(index);
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
-        
+
         public void MoveUpItem(int index)
         {
-            if(_array != null && index > 0)
+            if (_array != null && index > 0)
             {
                 _array.MoveArrayElement(index, index - 1);
                 bool expanded = _array.GetArrayElementAtIndex(index).isExpanded;
@@ -106,13 +188,13 @@ namespace VRLabs.ModularShaderSystem
                 _array.GetArrayElementAtIndex(index - 1).isExpanded = expanded;
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
-        
+
         public void MoveDownItem(int index)
         {
-            if(_array != null && index<_array.arraySize - 1)
+            if (_array != null && index < _array.arraySize - 1)
             {
                 _array.MoveArrayElement(index, index + 1);
                 bool expanded = _array.GetArrayElementAtIndex(index).isExpanded;
@@ -120,10 +202,10 @@ namespace VRLabs.ModularShaderSystem
                 _array.GetArrayElementAtIndex(index + 1).isExpanded = expanded;
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
-     
+
         // Add an item and refresh the list
         public void AddItem()
         {
@@ -132,7 +214,7 @@ namespace VRLabs.ModularShaderSystem
                 _array.InsertArrayElementAtIndex(_array.arraySize);
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
 
@@ -142,14 +224,14 @@ namespace VRLabs.ModularShaderSystem
             if (_array != null) _array.isExpanded = open;
             else _hasFoldingBeenForced = true;
         }
-     
+
         public new class UxmlFactory : UxmlFactory<InspectorList, UxmlTraits> { }
-     
+
         public new class UxmlTraits : BindableElement.UxmlTraits
         {
             UxmlBoolAttributeDescription showElements =
                 new UxmlBoolAttributeDescription { name = "show-elements-text", defaultValue = true };
-            
+
             public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
             {
                 base.Init(ve, bag, cc);
@@ -157,19 +239,48 @@ namespace VRLabs.ModularShaderSystem
                 if (ve is InspectorList ate) ate._showElementsButtons = showElements.GetValueFromBag(bag, cc);
             }
         }
-     
+
     }
-    
-    
-    
-    public class InspectorListItem : VisualElement {
+
+
+
+    public class InspectorListItem : VisualElement
+    {
         public Button removeButton;
         public Button upButton;
         public Button downButton;
-        public InspectorListItem(VisualElement element, SerializedProperty array, int index, bool showButtonsText)
+
+        public VisualElement dragArea;
+
+        public Vector2 startPosition;
+
+        public int index;
+
+        private IInspectorList _list;
+        public InspectorListItem(IInspectorList list, VisualElement element, SerializedProperty array, int index, bool showButtonsText)
         {
+            this.index = index;
+            _list = list;
             AddToClassList("inspector-list-item-container");
-            
+
+            dragArea = new VisualElement();
+            dragArea.AddToClassList("inspector-list-drag-handle");
+
+            dragArea.RegisterCallback<MouseDownEvent>(e =>
+            {
+                if (_list.draggedElement == this)
+                {
+                    e.StopImmediatePropagation();
+                    return;
+                }
+                else
+                {
+                    _list.draggedElement = this;
+                    _list.HighlightDrops();
+                    this.AddToClassList("inspector-list-drag-enabled");
+                }
+            });
+
             VisualElement buttonsArea = new VisualElement();
 
             this.RegisterCallback<UnityEngine.UIElements.GeometryChangedEvent>(e =>
@@ -190,7 +301,7 @@ namespace VRLabs.ModularShaderSystem
                     buttonsArea.Add(removeButton);
                 }
             });
-            
+
             upButton = new Button();
             upButton.name = "UpInspectorListItem";
             upButton.AddToClassList("inspector-list-up-button");
@@ -211,14 +322,13 @@ namespace VRLabs.ModularShaderSystem
                 downButton.text = "down";
                 removeButton.text = "-";
             }
-            
+
             var property = array.GetArrayElementAtIndex(index);
             element.AddToClassList("inspector-list-item");
-            
+
+            Add(dragArea);
             Add(element);
             Add(buttonsArea);
-           
-            
         }
     }
 }
--- Editor/Windows/ModuleInspectorList.cs
+++ Editor/Windows/ModuleInspectorList.cs
@@ -9,7 +9,7 @@ namespace VRLabs.ModularShaderSystem
 {
 
     // Shamelessly taken from here: https://forum.unity.com/threads/custom-bindableelement.989693/    
-    public class ModuleInspectorList : BindableElement
+    public class ModuleInspectorList : BindableElement, IInspectorList
     {
         Foldout _listContainer;
         Button _addButton;
@@ -19,23 +19,77 @@ namespace VRLabs.ModularShaderSystem
 
         private bool _hasFoldingBeenForced;
 
+        public InspectorListItem draggedElement { get; set; }
+        public bool _highlightDrops;
+
+        private List<VisualElement> _drops;
+
+        private VisualElement _currentDrop;
+
         public ModuleInspectorList()
         {
+            _drops = new List<VisualElement>();
             _listContainer = new Foldout();
             _listContainer.text = "Unbound List";
             _listContainer.contentContainer.AddToClassList("inspector-list-container");
             _listContainer.value = false;
+            _listContainer.RegisterCallback<MouseUpEvent>(e => Drop());
+            _listContainer.RegisterCallback<MouseLeaveEvent>(e => Drop());
+
             _addButton = new Button(AddItem);
             _addButton.text = "Add";
             _addButton.AddToClassList("inspector-list-add-button");
             Add(_listContainer);
-            if(enabledSelf)
+            if (enabledSelf)
                 _listContainer.Add(_addButton);
             _listContainer.RegisterValueChangedCallback((e) => _array.isExpanded = e.newValue);
-            var styleSheet = Resources.Load<StyleSheet>(MSSConstants.RESOURCES_FOLDER + "/MSSUIElements/ModuleInspectorList");
+            var styleSheet = Resources.Load<StyleSheet>(MSSConstants.RESOURCES_FOLDER + "/MSSUIElements/InspectorList");
             styleSheets.Add(styleSheet);
         }
-     
+
+        private void Drop()
+        {
+            if (draggedElement == null) return;
+            draggedElement.RemoveFromClassList("inspector-list-drag-enabled");
+
+            if (_highlightDrops)
+            {
+                DeHighlightDrops();
+                int dropIndex = _drops.IndexOf(_currentDrop);
+
+                if (dropIndex == -1)
+                {
+                    draggedElement = null;
+                    return;
+                }
+
+                if (dropIndex > draggedElement.index) dropIndex--;
+                _array.MoveArrayElement(draggedElement.index, dropIndex);
+                bool expanded = _array.GetArrayElementAtIndex(dropIndex).isExpanded;
+                _array.GetArrayElementAtIndex(dropIndex).isExpanded = _array.GetArrayElementAtIndex(draggedElement.index).isExpanded;
+                _array.GetArrayElementAtIndex(draggedElement.index).isExpanded = expanded;
+                _array.serializedObject.ApplyModifiedProperties();
+                UpdateList();
+            }
+            draggedElement = null;
+        }
+
+        public void HighlightDrops()
+        {
+            foreach (var item in _drops)
+                item.AddToClassList("inspector-list-drop-area-highlight");
+
+            _highlightDrops = true;
+        }
+
+        public void DeHighlightDrops()
+        {
+            foreach (var item in _drops)
+                item.RemoveFromClassList("inspector-list-drop-area-highlight");
+
+            _highlightDrops = false;
+        }
+
         // Get the reference to the bound serialized object.
         public override void HandleEvent(EventBase evt)
         {
@@ -54,24 +108,26 @@ namespace VRLabs.ModularShaderSystem
             }
             base.HandleEvent(evt);
         }
-     
+
         // Refresh/recreate the list.
         public void UpdateList()
         {
             _listContainer.Clear();
-           
+            _drops.Clear();
+
             if (_array == null)
                 return;
             _listContainer.text = _array.displayName;
+            CreateDrop();
 
             _loadedModules = new List<string>();
             for (int i = 0; i < _array.arraySize; i++)
             {
-                if(_array.GetArrayElementAtIndex(i).objectReferenceValue != null)
+                if (_array.GetArrayElementAtIndex(i).objectReferenceValue != null)
                     _loadedModules.Add(((ShaderModule)_array.GetArrayElementAtIndex(i).objectReferenceValue)?.Id);
             }
-                
-            
+
+
 
             for (int i = 0; i < _array.arraySize; i++)
             {
@@ -81,7 +137,7 @@ namespace VRLabs.ModularShaderSystem
                 var objectField = new ObjectField();//_array.GetArrayElementAtIndex(index));
 
                 SerializedProperty propertyValue = _array.GetArrayElementAtIndex(index);
-                
+
                 objectField.objectType = typeof(ShaderModule);
                 objectField.bindingPath = propertyValue.propertyPath;
                 objectField.Bind(propertyValue.serializedObject);
@@ -94,46 +150,63 @@ namespace VRLabs.ModularShaderSystem
                     var newValue = (ShaderModule)x.newValue;
                     var oldValue = (ShaderModule)x.previousValue;
 
-                    if(oldValue != null)
+                    if (oldValue != null)
                         _loadedModules.Remove(oldValue.Id);
-                    if(newValue != null)
+                    if (newValue != null)
                         _loadedModules.Add(newValue.Id);
 
                     for (int j = 0; j < _array.arraySize; j++)
                     {
                         var field = ((ObjectField)x.target).parent.parent.parent.ElementAt(j).ElementAt(0);
-                        Label label = field.ElementAt(1) as Label; 
-                        if(index == j)
+                        Label label = field.ElementAt(1) as Label;
+                        if (index == j)
                             CheckModuleValidity(newValue, label, field);
                         else
                             CheckModuleValidity((ShaderModule)_array.GetArrayElementAtIndex(j).objectReferenceValue, label, field);
                     }
                 });
-                    
-                var item = new InspectorListItem(moduleItem, _array, index, _showElementsButtons);
-                item.removeButton.RegisterCallback<PointerUpEvent>((evt) => 
-                {
-                    RemoveItem(index);
-                });
-                item.upButton.RegisterCallback<PointerUpEvent>((evt) =>
-                {
-                    MoveUpItem(index);
-                });
-                item.downButton.RegisterCallback<PointerUpEvent>((evt) => 
-                {
-                    MoveDownItem(index);
-                });
+
+                var item = new InspectorListItem(this, moduleItem, _array, index, _showElementsButtons);
+                item.removeButton.RegisterCallback<PointerUpEvent>((evt) => RemoveItem(index));
+                item.upButton.RegisterCallback<PointerUpEvent>((evt) => MoveUpItem(index));
+                item.downButton.RegisterCallback<PointerUpEvent>((evt) => MoveDownItem(index));
                 _listContainer.Add(item);
-                
+                CreateDrop();
+
                 CheckModuleValidity((ShaderModule)propertyValue.objectReferenceValue, infoLabel, moduleItem);
             }
-            if(enabledSelf)
+            if (enabledSelf)
                 _listContainer.Add(_addButton);
+        }
+
+        private void CreateDrop()
+        {
+            VisualElement dropArea = new VisualElement();
+            dropArea.AddToClassList("inspector-list-drop-area");
+            dropArea.RegisterCallback<MouseEnterEvent>(e =>
+            {
+                if (_highlightDrops)
+                {
+                    dropArea.AddToClassList("inspector-list-drop-area-selected");
+                    _currentDrop = dropArea;
+                }
+            });
+            dropArea.RegisterCallback<MouseLeaveEvent>(e =>
+            {
+                if (_highlightDrops)
+                {
+                    dropArea.RemoveFromClassList("inspector-list-drop-area-selected");
+                    if (_currentDrop == dropArea) _currentDrop = null;
+                }
+            });
+
+            _listContainer.Add(dropArea);
+            _drops.Add(dropArea);
         }
 
         private void CheckModuleValidity(ShaderModule newValue, Label infoLabel, VisualElement moduleItem)
         {
-            
+
             List<string> problems = new List<string>();
 
             if (newValue != null)
@@ -151,7 +224,7 @@ namespace VRLabs.ModularShaderSystem
                 if (incompatibilities.Count > 0)
                     problems.Add("These incompatible modules are installed: " + string.Join(", ", incompatibilities));
             }
-            
+
             infoLabel.text = string.Join("\n", problems);
 
             if (!string.IsNullOrWhiteSpace(infoLabel.text))
@@ -169,9 +242,9 @@ namespace VRLabs.ModularShaderSystem
         // Remove an item and refresh the list
         public void RemoveItem(int index)
         {
-            if(_array != null)
+            if (_array != null)
             {
-                if(index < _array.arraySize - 1)
+                if (index < _array.arraySize - 1)
                     _array.GetArrayElementAtIndex(index).isExpanded = _array.GetArrayElementAtIndex(index + 1).isExpanded;
                 var elementProperty = _array.GetArrayElementAtIndex(index);
                 if (elementProperty.objectReferenceValue != null)
@@ -179,13 +252,13 @@ namespace VRLabs.ModularShaderSystem
                 _array.DeleteArrayElementAtIndex(index);
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
-        
+
         public void MoveUpItem(int index)
         {
-            if(_array != null && index > 0)
+            if (_array != null && index > 0)
             {
                 _array.MoveArrayElement(index, index - 1);
                 bool expanded = _array.GetArrayElementAtIndex(index).isExpanded;
@@ -193,13 +266,13 @@ namespace VRLabs.ModularShaderSystem
                 _array.GetArrayElementAtIndex(index - 1).isExpanded = expanded;
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
-        
+
         public void MoveDownItem(int index)
         {
-            if(_array != null && index<_array.arraySize - 1)
+            if (_array != null && index < _array.arraySize - 1)
             {
                 _array.MoveArrayElement(index, index + 1);
                 bool expanded = _array.GetArrayElementAtIndex(index).isExpanded;
@@ -207,10 +280,10 @@ namespace VRLabs.ModularShaderSystem
                 _array.GetArrayElementAtIndex(index + 1).isExpanded = expanded;
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
-     
+
         // Add an item and refresh the list
         public void AddItem()
         {
@@ -219,7 +292,7 @@ namespace VRLabs.ModularShaderSystem
                 _array.InsertArrayElementAtIndex(_array.arraySize);
                 _array.serializedObject.ApplyModifiedProperties();
             }
-     
+
             UpdateList();
         }
 
@@ -229,14 +302,14 @@ namespace VRLabs.ModularShaderSystem
             if (_array != null) _array.isExpanded = open;
             else _hasFoldingBeenForced = true;
         }
-     
+
         public new class UxmlFactory : UxmlFactory<ModuleInspectorList, UxmlTraits> { }
-     
+
         public new class UxmlTraits : BindableElement.UxmlTraits
         {
             UxmlBoolAttributeDescription showElements =
                 new UxmlBoolAttributeDescription { name = "show-elements-text", defaultValue = true };
-            
+
             public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
             {
                 base.Init(ve, bag, cc);
